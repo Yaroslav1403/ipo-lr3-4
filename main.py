64f06e5d6b3348655c08f1149f98c40f2192f340
Yaroslav <122485322+Yaroslav1403@users.noreply.github.com>
2024-10-19 09:15:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import math
 x = float(input("Введите значение x:"))
 #Вычисление f(x) в зависимости от условия
 if x < 0:
-    result = x ** 2  #условие f(x) = x² при x < 0
+#Условие f(x) = x² при x < 0    
+    result = x ** 2  
 else:
-#условие f(x) = √x при x ≥ 0    
+#Условие f(x) = √x при x ≥ 0    
     result = math.sqrt(x) 
 #Вывод результата
 print(f"f({x}) = {result}")
